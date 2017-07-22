7c60843b636ce1b3b50390f7460dcc82a80058cc
yxy <yxy.870@gmail.com>
2017-07-23 02:41:46 +0800

    added modeladmin

--- crm/admin.py
+++ crm/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 
-from .models import Company
+from .models import Company, People, ShareHolder, Contract
 
 
 @admin.register(Company)
 class CompanyModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Contract)
+class ContractModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(People)
+class PeopleModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ShareHolder)
+class ShareHolderModelAdmin(admin.ModelAdmin):
     pass
