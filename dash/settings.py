2200aaa9d81ed6fa3fc9b6854933b22d2b00ed11
yxy <yxy.870@gmail.com>
2017-07-25 00:54:53 +0800

    密码校验降低难度

--- dash/settings.py
+++ dash/settings.py
@@ -77,19 +77,19 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-            'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME':
-            'django.contrib.auth.password_validation.MinimumLengthValidator', },
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
     {
-        'NAME':
-            'django.contrib.auth.password_validation.CommonPasswordValidator', },
-    {
-        'NAME':
-            'django.contrib.auth.password_validation.NumericPasswordValidator',
-    }, ]
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    # {
+    #     'NAME':
+    #         'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
