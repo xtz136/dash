1b8f89828f954bb50c962b12843b87596031b37f
yxy <yxy.870@gmail.com>
2017-08-24 11:24:16 +0800

    增加raven app

--- dash/settings.py
+++ dash/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'raven.contrib.django.raven_compat',
     'core',
     'crm',
     'security'
