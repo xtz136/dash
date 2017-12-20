79fd32be98dafb7d1f7cc437b97df36855f96665
yxy <yxy.870@gmail.com>
2017-12-21 01:43:46 +0800

    added application model

---
+++ core/models/apply.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.conf import settings
+
+from crm.models import Company
+
+from django_fsm import FSMField, transition
+
+
+class Application(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    name = models.CharField(max_length=200)
+    company_title = models.CharField(max_length=200)
+    info = models.TextField(blank=True)
+    phone = models.CharField('phone', max_length=50)
+    company = models.ForeignKey(Company, blank=True, null=True)
+    state = FSMField(default='new')
+
+    @transition(field=state, source='new', target='approved')
+    def approve(self):
+        self.user.profile.company = self.company
+        self.user.is_active = True
+        self.user.save()
+        # send notify
+
+    @transition(field=state, source='new', target='denied')
+    def deny(self):
+        pass
+        # send notify
