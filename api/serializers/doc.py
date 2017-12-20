e8cd520dbf3c137b1803dac1bd50284605fc5cce
yxy <yxy.870@gmail.com>
2017-12-20 14:28:19 +0800

    added folder validator

--- api/serializers/doc.py
+++ api/serializers/doc.py
@@ -6,6 +6,8 @@ from rest_framework import serializers
 from project.models import Folder, File
 from .user import UserSerializer
 from .tag import TagSerializer
+from rest_framework.validators import UniqueForYearValidator
+
 
 User = get_user_model()
 
@@ -15,6 +17,12 @@ class FolderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Folder
         fields = ('id', 'name', 'parent', 'project')
+
+    def validate(self, data):
+        if Folder.objects.filter(**data).exists():
+            raise serializers.ValidationError(
+                'folder with name {0} already existed'.format(data['name']))
+        return data
 
 
 class FileSerializer(serializers.ModelSerializer):
