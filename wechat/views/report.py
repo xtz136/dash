90d1b8ebaf0f1130a94e3d4aeb8f458e90322885
yxy <yxy.870@gmail.com>
2017-12-28 12:21:00 +0800

    added media url

--- wechat/views/report.py
+++ wechat/views/report.py
@@ -12,7 +12,6 @@ class ReportListView(ListView):
     def get_queryset(self):
         queryset = super(ReportListView, self).get_queryset()
         queryset = queryset.filter(company=self.request.user.profile.company)
-        print(queryset)
         return queryset
 
     def get_context_data(self):
