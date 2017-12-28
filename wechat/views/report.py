a1d3c5153d9ee30e8b9765225fc0d7b470412cbb
yxy <yxy.870@gmail.com>
2017-12-28 12:15:55 +0800

    报表列表

--- wechat/views/report.py
+++ wechat/views/report.py
@@ -7,3 +7,15 @@ from accounting.models import Report
 class ReportListView(ListView):
 
     model = Report
+    template_name = 'wechat/report_list.html'
+
+    def get_queryset(self):
+        queryset = super(ReportListView, self).get_queryset()
+        queryset = queryset.filter(company=self.request.user.profile.company)
+        print(queryset)
+        return queryset
+
+    def get_context_data(self):
+        data = super(ReportListView, self).get_context_data()
+        data['company'] = self.request.user.profile.company
+        return data
