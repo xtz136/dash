6e028c3cc2e8290795c1c9ed0bdff0bd7e75d864
yxy <yxy.870@gmail.com>
2017-12-28 16:10:56 +0800

    ui改进

--- wechat/utils.py
+++ wechat/utils.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from rest_framework_jwt.settings import api_settings
 
 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
@@ -10,3 +11,10 @@ def issue_token(user):
         'token': jwt_encode_handler(payload),
         'user': user
     }
+
+
+def validate_month(s):
+    try:
+        return datetime.strptime(s, '%Y-%m')
+    except Exception:
+        return None
--- wechat/views/report.py
+++ wechat/views/report.py
@@ -2,6 +2,7 @@ from django.views.generic.list import ListView
 from django.utils import timezone
 
 from accounting.models import Report
+from ..utils import validate_month
 
 
 class ReportListView(ListView):
@@ -11,10 +12,15 @@ class ReportListView(ListView):
 
     def get_queryset(self):
         queryset = super(ReportListView, self).get_queryset()
-        queryset = queryset.filter(company=self.request.user.profile.company)
-        return queryset
+        lookups = {'company': self.request.user.profile.company}
+        month = validate_month(self.request.GET.get('month', ''))
+        if month:
+            lookups['date__month'] = month.month
+            lookups['date__year'] = month.year
+        return queryset.filter(**lookups)
 
     def get_context_data(self):
         data = super(ReportListView, self).get_context_data()
         data['company'] = self.request.user.profile.company
+        data['month'] = self.request.GET.get('month')
         return data
