0f5c363bbfd5ab0c1e0b71140eb294d1916e1adf
xtz <xutaozhe@126.com>
2019-01-03 07:18:47 +0800

    导出字段改为中文

--- crm/urls.py
+++ crm/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib.auth import views as auth_views
-from . import views
+
+from . import hack_export_action, views
 
 urlpatterns = [
     url(r'^accounts/register/$',
@@ -13,15 +14,19 @@ urlpatterns = [
         auth_views.LogoutView.as_view(template_name='crm/logout.html'),
         name='logout'),
     url(r'^dashboard/$', views.DashboardView.as_view(), name='dashboard'),
-
     url(r'^client/batch/$',
-        views.BatchClientUpdateView.as_view(), name='client-batch'),
+        views.BatchClientUpdateView.as_view(),
+        name='client-batch'),
     url(r'^client/search/$',
-        views.ClientSearchView.as_view(), name='client-search'),
+        views.ClientSearchView.as_view(),
+        name='client-search'),
     url(r'^client/create/$',
-        views.ClientCreateView.as_view(), name='client-create'),
+        views.ClientCreateView.as_view(),
+        name='client-create'),
     url(r'^client/(?P<pk>\d+)/$',
-        views.ClientDetailView.as_view(), name='client-detail'),
+        views.ClientDetailView.as_view(),
+        name='client-detail'),
     url(r'^client/(?P<pk>\d+)/edit/$',
-        views.ClientEditView.as_view(), name='client-edit'),
+        views.ClientEditView.as_view(),
+        name='client-edit'),
 ]
